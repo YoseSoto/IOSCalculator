d6ab07b0dd89ac252b81917cd927d066f54039ba
YoseSoto <sotomayorm74@gmail.com>
2023-07-11 16:38:23 -0600

    finished

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const numbers = document.querySelectorAll(".numbers");
+const operator = document.querySelectorAll(".operator");
 const result = document.querySelector(".result span");
 const signs = document.querySelectorAll(".sign");
 const equals = document.querySelector(".equals");
@@ -9,6 +10,7 @@ const times = document.querySelector(".times");
 const minus = document.querySelector(".minus");
 const plus = document.querySelector(".plus");
 const dot = document.querySelector(".dot");
+const AC = document.querySelector(".AC");
 
 let firstValue = "";
 let isFirstValue = false;
@@ -16,6 +18,25 @@ let secondValue = "";
 let isSecondValue = false;
 let sign = "";
 let resultValue = 0;
+let isActive = false;
+
+function deleteActive(){
+  operator.forEach((op1) => {
+    op1.classList.remove("active");
+  });
+}
+
+function changeActive() {
+  operator.forEach((op) => {
+    op.addEventListener("click", () => {
+      deleteActive();
+      isActive = !isActive;
+      op.classList.add("active");
+    });
+  });
+}
+
+changeActive();
 
 numbers.forEach((button) => {
   button.addEventListener("click", (e) => {
@@ -32,8 +53,9 @@ numbers.forEach((button) => {
 function getFirstValue(el) {
   result.innerHTML = "";
   firstValue += el;
-  result.innerHTML = firstValue;
   firstValue = +firstValue;
+  result.innerHTML = firstValue;
+  
 }
 
 function getSecondValue(el) {
@@ -41,6 +63,7 @@ function getSecondValue(el) {
     secondValue += el;
     result.innerHTML = secondValue;
     secondValue = +secondValue;
+    
   }
 }
 
@@ -55,9 +78,74 @@ function getSign() {
 getSign();
 
 equals.addEventListener("click", () => {
-    result.innerHTML = "";
-    if (sign === "+") {
-      resultValue = firstValue + secondValue;
-    }
+  deleteActive();
+  result.innerHTML = "";
+  if (sign === "+") {
+    resultValue = firstValue + secondValue;
+  } else if (sign === "-") {
+    resultValue = firstValue - secondValue;
+  } else if (sign === "x") {
+    resultValue = firstValue * secondValue;
+  } else if (sign === "/") {
+    resultValue = firstValue / secondValue;
+  }
+  if (!isNaN(resultValue) && (resultValue !== "") && isFinite(resultValue)) {
     result.innerHTML = resultValue;
-  });
+  }
+  else{
+    result.innerHTML = "Error";
+  }
+  firstValue = resultValue;
+  secondValue = "";
+
+  checkResultLenght();
+
+});
+
+AC.addEventListener("click", () =>{
+  result.innerHTML = 0;
+  firstValue = "";
+  isFirstValue = false;
+  secondValue = "";
+  isSecondValue = false
+  sign = "";
+  resultValue = 0;
+  isActive = false;
+});
+
+plusMinus.addEventListener("click", () => {
+  result.innerHTML = "";
+  if (firstValue != "") {
+    resultValue = -firstValue;
+    firstValue = resultValue;
+  }
+  if (firstValue != "" && secondValue != "" && sign != "" ) {
+    resultValue = -resultValue;
+  }
+
+  result.innerHTML = resultValue;
+
+});
+
+percent.addEventListener("click", () => {
+  result.innerHTML = "";
+  if (firstValue != "") {
+    resultValue = firstValue/100;
+    firstValue = resultValue;
+  }
+  if (firstValue != "" && secondValue != "" && sign != "" ) {
+    resultValue = resultValue/100;
+  }
+
+  result.innerHTML = resultValue;
+});
+ 
+
+function checkResultLenght(){
+  resultValue = JSON.stringify(resultValue);
+
+  if (resultValue.length >= 8) {
+    resultValue = JSON.parse(resultValue);
+    result.innerHTML = resultValue.toFixed(5);
+  }
+}
